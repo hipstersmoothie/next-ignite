3047678b90bf9f80f74509a5a5f869238736b63c
Andrew Lisowski <lisowski54@gmail.com>
2020-03-12 12:29:43 -0700

    add search

--- components/navbar.js
+++ components/navbar.js
@@ -1,20 +1,58 @@
 import Link from "next/link";
+import searchIndex from "../search.json";
 
 import { Anchor } from "./anchor";
+import { formatPath } from "../utils/format-path";
 
-export const NavBar = () => (
-  <div className="flex justify-between h-12">
-    <Link href="/">
-      <Anchor>Home</Anchor>
-    </Link>
+const Search = () => {
+  const [search, setSearch] = React.useState("");
+  const normalizedSearch = search.toLowerCase();
+  const results = searchIndex.filter(
+    page =>
+      page.content?.toLowerCase().includes(search) ||
+      page.title?.toLowerCase().includes(search) ||
+      page.author?.toLowerCase().includes(search)
+  );
 
-    <div className="flex">
-      <Link href="/docs">
-        <Anchor className="pr-4">Docs</Anchor>
-      </Link>
-      <Link href="/blog">
-        <Anchor>Blog</Anchor>
-      </Link>
+  return (
+    <div className="relative">
+      <input
+        placeholder="Search"
+        value={search}
+        onChange={e => {
+          setSearch(e.currentTarget.value);
+        }}
+      />
+
+      {search && (
+        <div className="absolute">
+          {results.map(result => (
+            <Anchor href={formatPath(result.__resourcePath)}>
+              {result.title}
+            </Anchor>
+          ))}
+        </div>
+      )}
     </div>
-  </div>
-);
+  );
+};
+
+export const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center h-12 px-6">
+      <Link href="/">
+        <Anchor>Home</Anchor>
+      </Link>
+
+      <div className="flex">
+        <Search />
+        <Link href="/docs">
+          <Anchor className="pr-4">Docs</Anchor>
+        </Link>
+        <Link href="/blog">
+          <Anchor>Blog</Anchor>
+        </Link>
+      </div>
+    </div>
+  );
+};
--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-const withMdxEnhanced = require("next-mdx-enhanced")();
+const fs = require("fs");
+
+const isProduction = process.env.NODE_ENV === "production";
+const pages = [];
+
+if (isProduction) {
+  fs.writeFileSync("search.json", JSON.stringify([]));
+}
+\
+const withMdxEnhanced = require("next-mdx-enhanced")({
+  onContent: page => {
+    if (isProduction) {
+      pages.push(page);
+      fs.writeFileSync("search.json", JSON.stringify(pages));
+    }
+  }
+});
 
 module.exports = withMdxEnhanced({});
