8c55c939c0e7f058d72661d5d0f6c7ac74a6f0a8
Andrew Lisowski <lisowski54@gmail.com>
2020-03-12 15:16:44 -0700

    dynimic blog post loading

--- pages/blog.js
+++ pages/blog.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
-import makeNavBarLayout from "../layouts/nav-bar";
+import path from "path";
 
+import makeNavBarLayout from "../layouts/nav-bar";
 import { formatPath } from "../utils/format-path";
-import { frontMatter as firstPost } from "../pages/blog/first.mdx";
-import { frontMatter as secondPost } from "../pages/blog/second.mdx";
 import { Anchor } from "../components/anchor";
 
 const NavBarLayout = makeNavBarLayout();
+const posts = require.context("../pages/blog/", true, /\.mdx$/).keys();
+const requireFrontMatters = require.context("../.mdx-data", true, /\.json$/);
+const frontMatters = requireFrontMatters.keys().map(requireFrontMatters);
 
 const BlogPostList = () => {
-  const blogPosts = [firstPost, secondPost];
+  const blogPosts = posts
+    .map(key => path.relative("./", key))
+    .map(key =>
+      frontMatters.find(f => f.__resourcePath === path.join("blog", key))
+    );
 
   return (
     <NavBarLayout>
