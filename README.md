1a769f0e1293db1dfa93771d4cca0ecdad0280df
Andrew Lisowski <lisowski54@gmail.com>
2020-03-12 14:45:22 -0700

    determine layout based on file, default to docs layout for undefined

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - Add multiple top level docs sections just by adding more folders and mdx in  `pages/`
 - Home Page (Break out into JS easy as hell)
 - Simple search (only work after a full production build)
+- Dynamically match `layouts` + add fallbacks based on routes
 
 ## Getting Started
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 const isProduction = process.env.NODE_ENV === "production";
 const pages = [];
@@ -13,6 +14,34 @@ const withMdxEnhanced = require("next-mdx-enhanced")({
       pages.push(page);
       fs.writeFileSync("search.json", JSON.stringify(pages));
     }
+  },
+  extendFrontMatter: {
+    process: (_, frontMatter) => {
+      const { __resourcePath, layout } = frontMatter;
+
+      if (!layout) {
+        const defaultLayout = __resourcePath.split("/")[0];
+
+        if (defaultLayout === 'blog') {
+          return {
+            layout: 'nav-bar'
+          };
+        } else if (
+          fs.existsSync(
+            path.join(process.cwd(), `layouts/${defaultLayout}.tsx`)
+          )
+        ) {
+          return {
+            layout: defaultLayout
+          };
+        } else {
+          return {
+            layout: 'docs'
+          };
+        }
+      }
+    },
+    phase: "both"
   }
 });
 
