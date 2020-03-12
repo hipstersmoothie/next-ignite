f29c0183083de9162bd6c32868ddf68883494af5
Andrew Lisowski <lisowski54@gmail.com>
2020-03-12 12:32:50 -0700

    woah

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import makeNavBarLayout from "../layouts/navbar";
-
-const NavBarLayout = makeNavBarLayout();
-
-export default () => (
-  <NavBarLayout>
-    <h1>Home Page</h1>
-  </NavBarLayout>
-);
