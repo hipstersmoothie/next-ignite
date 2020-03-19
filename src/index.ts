181b3b86ce3dda8cad57867d6c5f4f3557f81fcf
Andrew Lisowski <lisowski54@gmail.com>
2020-03-19 15:51:15 -0700

    fix build when no blog

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { default as getBlogPosts } from "./utils/get-blog-posts";
 export { default as makeNavBarLayout } from "./layouts/nav-bar";
 export { default as igniteComponents } from "./components/mdx-components";
 export { default as BlogIndex } from "./components/blog-index";
--- src/utils/get-blog-posts.ts
+++ src/utils/get-blog-posts.ts
@@ -6,21 +6,26 @@ import { BlogPost } from "./types";
 declare var PAGES_DIR: string;
 declare var MDX_DATA_DIR: string;
 
-const posts = require.context(PAGES_DIR + "/blog/", true, /\.mdx$/).keys();
-const requireFrontMatters = require.context(MDX_DATA_DIR, true, /\.json$/);
-const frontMatters = requireFrontMatters
-  .keys()
-  .map(requireFrontMatters) as BlogPost[];
-const blogPosts = posts
-  .map(key => path.relative("./", key))
-  .map(key =>
-    frontMatters.find(f => f.__resourcePath === path.join("blog", key))
-  )
-  .map(post => ({
-    ...post,
-    __resourcePath: formatPath(post.__resourcePath)
-  }))
-  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
-
 /** Get all the blog posts in the project */
-export default () => blogPosts;
+export default () => {
+  try {
+    const posts = require.context(PAGES_DIR + "/blog/", true, /\.mdx$/).keys();
+    const requireFrontMatters = require.context(MDX_DATA_DIR, true, /\.json$/);
+    const frontMatters = requireFrontMatters
+      .keys()
+      .map(requireFrontMatters) as BlogPost[];
+
+    return posts
+      .map(key => path.relative("./", key))
+      .map(key =>
+        frontMatters.find(f => f.__resourcePath === path.join("blog", key))
+      )
+      .map(post => ({
+        ...post,
+        __resourcePath: formatPath(post.__resourcePath)
+      }))
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  } catch (error) {
+    return []
+  }
+};
