1c4bd14aa5be02305edbc468c72182056cba8657
Andrew Lisowski <lisowski54@gmail.com>
2020-06-25 09:57:19 -0700

    load favicon

--- src/layouts/nav-bar.tsx
+++ src/layouts/nav-bar.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Head from "next/head";
+import path from 'path';
 
 import { NavBar } from "../components/navbar";
 import { MobileMenuContext } from "../utils/mobile-menu-context";
+import { formatPath } from "../utils/format-path";
 
+declare const FAVICON: string;
 declare const HAS_HOMEPAGE: boolean;
 declare const TOP_LEVEL_SECTIONS: string[];
 
@@ -41,6 +45,10 @@ export default () => ({
 
   return (
     <MobileMenuContext.Provider value={props.menuState || menuState}>
+      <Head>
+        <link rel="shortcut icon" href={formatPath(path.join('images', FAVICON))} />
+      </Head>
+
       <div id="ignite" className="min-h-screen flex flex-col dark:bg-gray-1000">
         <NavBar sections={TOP_LEVEL_SECTIONS} hasHomePage={HAS_HOMEPAGE} />
         {content}
