f0fcae7714a0431033ea34fc76b8bbbbf33bcb75
Andrew Lisowski <lisowski54@gmail.com>
2020-07-06 13:00:52 -0700

    print correct dev url

--- bin/ignite.js
+++ bin/ignite.js
@@ -71,7 +71,7 @@ if (args._command === "dev") {
         throw err;
       }
 
-      console.log("> Ready on http://localhost:3000");
+      console.log("> Ready on http://localhost:3000/docs");
     });
   });
 }
