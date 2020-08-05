af9b3b20b7e5d7d905ab1de51b3db8b1fa2426b6
Andrew Lisowski <lisowski54@gmail.com>
2020-08-05 11:26:19 -0700

    enable purgecss is prod mode

--- src/next.js
+++ src/next.js
@@ -175,87 +175,86 @@ const DEFAULT_LOGO = "https://hipstersmoothie.github.io/next-ignite/logo.svg";
 // name - The name of the project you are documenting
 // repo - The repo the documentation is for
 // order - string array of top level section order
-module.exports = (igniteConfig = {}) =>
-  function (nextConfig = {}) {
-    const debug = process.env.NODE_ENV !== "production";
-    const { pathname, ...rest } = igniteConfig.url
-      ? new URL(igniteConfig.url)
-      : { pathname: "/" };
-    const BASE_PATH = debug
-      ? ""
-      : pathname.endsWith("/")
-      ? pathname.slice(0, -1)
-      : pathname;
-    const favicon = glob.sync(path.join(publicDir, "**/favicon.*"))[0];
-    const faviconDark = glob.sync(path.join(publicDir, "**/favicon-dark.*"))[0];
-    const logo = glob.sync(path.join(publicDir, "**/logo.*"))[0];
-    const darkLogo = glob.sync(path.join(publicDir, "**/logo-dark.*"))[0];
+module.exports = (igniteConfig = {}) => (nextConfig = {}) => {
+  const debug = process.env.NODE_ENV !== "production";
+  const { pathname, ...rest } = igniteConfig.url
+    ? new URL(igniteConfig.url)
+    : { pathname: "/" };
+  const BASE_PATH = debug
+    ? ""
+    : pathname.endsWith("/")
+    ? pathname.slice(0, -1)
+    : pathname;
+  const favicon = glob.sync(path.join(publicDir, "**/favicon.*"))[0];
+  const faviconDark = glob.sync(path.join(publicDir, "**/favicon-dark.*"))[0];
+  const logo = glob.sync(path.join(publicDir, "**/logo.*"))[0];
+  const darkLogo = glob.sync(path.join(publicDir, "**/logo-dark.*"))[0];
 
-    return withPurgeCss(
-      withBundleAnalyzer(
-        withMdxEnhanced({
-          ...nextConfig,
-          purgeCssEnabled: ({ dev, isServer }) => false,
-          purgeCssPaths: [
-            path.relative(
-              path.join(process.cwd(), "docs"),
-              path.join(DOCS_DIR, "**/*")
-            ),
-            path.relative(
-              path.join(process.cwd(), "docs"),
-              path.join(__dirname, "**/*")
-            ),
-          ],
-          experimental: {
-            basePath: BASE_PATH,
-          },
-          publicRuntimeConfig: {
-            assetPrefix: BASE_PATH,
-          },
-          webpack: (
-            config,
-            { buildId, dev, isServer, defaultLoaders, webpack }
-          ) => {
-            config.plugins.push(
-              new webpack.DefinePlugin({
-                BASE_PATH: JSON.stringify(debug ? "/" : BASE_PATH || "/"),
-                PROJECT_NAME: JSON.stringify(igniteConfig.name),
-                FAVICON: JSON.stringify(
-                  favicon ? path.relative(publicDir, favicon) : ""
-                ),
-                FAVICON_DARK: JSON.stringify(
-                  faviconDark ? path.relative(publicDir, faviconDark) : ""
-                ),
-                PROJECT_LOGO: JSON.stringify(
-                  logo ? path.relative(publicDir, logo) : DEFAULT_LOGO
-                ),
-                PROJECT_LOGO_DARK: JSON.stringify(
-                  darkLogo ? path.relative(publicDir, darkLogo) : DEFAULT_LOGO
-                ),
-                REPO_URL: JSON.stringify(getFullGitHubUrl(igniteConfig.repo)),
-                PAGES_DIR: JSON.stringify(PAGES_DIR),
-                PAGES: JSON.stringify(
-                  getPages().map((p) => path.relative(PAGES_DIR, p))
-                ),
-                BLOG_POSTS: JSON.stringify(
-                  getBlogPosts().map((p) => path.relative(PAGES_DIR, p))
-                ),
-                MDX_DATA_DIR: JSON.stringify(MDX_DATA_DIR),
-                HAS_HOMEPAGE: JSON.stringify(getHasHomepage()),
-                TOP_LEVEL_SECTIONS: JSON.stringify(
-                  getTopLevelSections(igniteConfig.order)
-                ),
-              })
-            );
+  return withPurgeCss(
+    withBundleAnalyzer(
+      withMdxEnhanced({
+        ...nextConfig,
+        purgeCssEnabled: ({ dev }) => !dev,
+        purgeCssPaths: [
+          path.relative(
+            path.join(process.cwd(), "docs"),
+            path.join(DOCS_DIR, "**/*")
+          ),
+          path.relative(
+            path.join(process.cwd(), "docs"),
+            path.join(__dirname, "**/*")
+          ),
+        ],
+        experimental: {
+          basePath: BASE_PATH,
+        },
+        publicRuntimeConfig: {
+          assetPrefix: BASE_PATH,
+        },
+        webpack: (
+          config,
+          { buildId, dev, isServer, defaultLoaders, webpack }
+        ) => {
+          config.plugins.push(
+            new webpack.DefinePlugin({
+              BASE_PATH: JSON.stringify(debug ? "/" : BASE_PATH || "/"),
+              PROJECT_NAME: JSON.stringify(igniteConfig.name),
+              FAVICON: JSON.stringify(
+                favicon ? path.relative(publicDir, favicon) : ""
+              ),
+              FAVICON_DARK: JSON.stringify(
+                faviconDark ? path.relative(publicDir, faviconDark) : ""
+              ),
+              PROJECT_LOGO: JSON.stringify(
+                logo ? path.relative(publicDir, logo) : DEFAULT_LOGO
+              ),
+              PROJECT_LOGO_DARK: JSON.stringify(
+                darkLogo ? path.relative(publicDir, darkLogo) : DEFAULT_LOGO
+              ),
+              REPO_URL: JSON.stringify(getFullGitHubUrl(igniteConfig.repo)),
+              PAGES_DIR: JSON.stringify(PAGES_DIR),
+              PAGES: JSON.stringify(
+                getPages().map((p) => path.relative(PAGES_DIR, p))
+              ),
+              BLOG_POSTS: JSON.stringify(
+                getBlogPosts().map((p) => path.relative(PAGES_DIR, p))
+              ),
+              MDX_DATA_DIR: JSON.stringify(MDX_DATA_DIR),
+              HAS_HOMEPAGE: JSON.stringify(getHasHomepage()),
+              TOP_LEVEL_SECTIONS: JSON.stringify(
+                getTopLevelSections(igniteConfig.order)
+              ),
+            })
+          );
 
-            // Don't clobber previous plugins' webpack functions
-            if (typeof nextConfig.webpack === "function") {
-              return nextConfig.webpack(config, options);
-            }
+          // Don't clobber previous plugins' webpack functions
+          if (typeof nextConfig.webpack === "function") {
+            return nextConfig.webpack(config, options);
+          }
 
-            return config;
-          },
-        })
-      )
-    );
-  };
+          return config;
+        },
+      })
+    )
+  );
+};
