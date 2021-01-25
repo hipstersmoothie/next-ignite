72fee69d24b7c48263b46a03d67dccd865d9a91e
Andrew Lisowski <lisowski54@gmail.com>
2021-01-25 10:18:49 -0800

    create a PWA if manifest.json is detected

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -15,6 +15,39 @@ class MyDocument extends Document {
             href="https://www.unpkg.com/prism-themes@1.5.0/themes/prism-coldark-dark.css"
             media="(prefers-color-scheme: dark)"
           />
+
+          <meta name="application-name" content="next-ignite Docs" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="next-ignite Docs" />
+          <meta
+            name="description"
+            content="The documentation for next-ignite"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#11151d" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#3b82f6" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://hipstersmoothie.github.io/next-ignite/" />
+          <meta name="twitter:title" content="next-ignite Docs" />
+          <meta
+            name="twitter:description"
+            content="The documentation for next-ignite"
+          />
+          <meta name="twitter:creator" content="@hipstersmoothie" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="PWA App" />
+          <meta property="og:description" content="The documentation for next-ignite" />
+          <meta property="og:site_name" content="PWA App" />
+          <meta property="og:url" content="https://hipstersmoothie.github.io/next-ignite/" />
         </Head>
         <body>
           <Main />
