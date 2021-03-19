7d506ca3b2a0e389b5492fd7dff840c9a9e56384
Andrew Lisowski <lisowski54@gmail.com>
2021-03-19 14:49:23 -0700

    add "none" layout

---
+++ src/layouts/none.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+/** just render the mdx content */
+const NavBarLayout = ({ children }: React.PropsWithChildren<{}>) => {
+  return children;
+};
+
+export default NavBarLayout;
